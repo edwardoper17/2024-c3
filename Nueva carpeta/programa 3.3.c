51b546f299ccfeb2b1efa593aef7db27c819e964
edwardoper17 <arielperez-17@outlook.com>
2024-10-18 09:59:27 -0400

    Add files via upload

---
+++ Nueva carpeta/programa 3.3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/* Suma pagos.
+El programa, al recibir como datos un conjunto de pagos realizados en el último
+ ➥mes, obtiene la suma de los mismos.
+
+ PAG y SPA: variables de tipo real.  */
+
+ void main(void)
+{
+float PAG, SPA;
+SPA = 0;
+prointf('Ingrese el primer pago:\t');
+scanf('%f', &PAG);
+while (PAG)
+/* Observa que la proposición que modifica la condición es una lectura. */
+
+}
+prinft('\nEl total de pagos del mes es: %.2f',SPA);
+}
+
+
